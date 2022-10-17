a83885308a9d5834337bab16cff47e76cbdfcb38
BoostedBanobro <72575802+BoostedBanobro@users.noreply.github.com>
2022-10-16 20:07:25 -0700

    added tutorial

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorial.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class tutorial extends OpMode {
+    DcMotor motor;
+    @Override
+    public void init() {
+        motor = hardwareMap.get(DcMotor.class, "motor");
+        telemetry.addData("Hardware: ", "Initialized");
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.left_stick_y > 0){
+            motor.setPower(0.5);
+        }
+        motor.setPower(0);
+
+    }
+}
