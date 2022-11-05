12076c83324c07691002ffb464e844192e7b3244
BoostedBanobro <72575802+BoostedBanobro@users.noreply.github.com>
2022-11-04 21:52:06 -0700

    servos done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/tutorial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/tutorial.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+@TeleOp
 public class tutorial extends OpMode {
     DcMotor motor;
     @Override
