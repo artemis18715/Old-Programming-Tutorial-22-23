38269b5eae90dc2828d86ff9ed2bf177caf73b02
Arthur Nguyen <arthurn3591@gmail.com>
2023-08-26 08:49:49 -0700

    fixed bugs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/motor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/motor.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp
-public class tutorial extends OpMode {
+public class motor extends OpMode {
     DcMotor motor;
     @Override
     public void init() {
